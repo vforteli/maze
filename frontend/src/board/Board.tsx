2ed9c2d625bf5fc962f1f603dcb6afdf84f28cce
Verner Fortelius <verner@fortelius.net>
2024-08-27 22:45:13 +0300

    add board frame and some rotation animation stuff

--- frontend/src/board/Board.tsx
+++ frontend/src/board/Board.tsx
@@ -1,6 +1,6 @@
 import "./Board.scss";
-import React from "react";
-import { CellMemoized, CellProps, directions } from "./Cell";
+import React, { useMemo, useState } from "react";
+import { CellMemoized, CellProps, Direction, directions } from "./Cell";
 import { getRandomInteger, shuffleArray } from "../utils";
 import { fixedCells, movableCells } from "./Cells";
 
@@ -26,15 +26,30 @@ function getRandomBoardCells(): { cells: CellProps[]; playerCell: CellProps } {
   return { cells: cells, playerCell: { ...randomCells.pop()!, rotation: 0 } };
 }
 
+function rotateRight(direction: Direction): Direction {
+  switch (direction) {
+    case 0:
+      return 90;
+    case 90:
+      return 180;
+    case 180:
+      return 270;
+    case 270:
+      return 0;
+  }
+}
+
 const Board = () => {
-  const { cells, playerCell } = getRandomBoardCells();
-  console.debug(playerCell);
+  const { cells, playerCell } = useMemo(() => getRandomBoardCells(), []);
+  const [playerCellDirection, setPlayerCellDirection] = useState<Direction>(0);
 
   return (
     <div className="board-container">
       <div className="tile-test">
         <div style={{ width: 100, margin: 20 }}>
-          <CellMemoized {...playerCell} />
+          <div onClick={() => setPlayerCellDirection(rotateRight(playerCellDirection))}>
+            <CellMemoized {...playerCell} rotation={playerCellDirection} />
+          </div>
         </div>
         <div style={{ width: 100, margin: 20 }}>
           <CellMemoized type="T" rotation={0} />
@@ -46,10 +61,12 @@ const Board = () => {
           <CellMemoized type="I" rotation={0} />
         </div>
       </div>
-      <div className="board">
-        {cells.map((o, i) => (
-          <CellMemoized key={i} {...o} />
-        ))}
+      <div className="board-frame">
+        <div className="board">
+          {cells.map((o, i) => (
+            <CellMemoized key={i} {...o} />
+          ))}
+        </div>
       </div>
     </div>
   );
