d61b6ebc55f79f447311ba30f06548cac3ceff64
Verner Fortelius <verner@fortelius.net>
2024-08-27 16:49:39 +0300

    add some well known cells

--- frontend/src/board/Board.tsx
+++ frontend/src/board/Board.tsx
@@ -1,23 +1,45 @@
 import "./Board.scss";
 import React from "react";
-import { CellMemoized, directions } from "./Cell";
+import { CellMemoized, CellProps, directions } from "./Cell";
 import { getRandomInteger, shuffleArray } from "../utils";
 import { fixedCells, movableCells } from "./Cells";
 
 export type BoardProps = unknown;
 
-const Board = () => {
+function getRandomBoardCells(): { cells: CellProps[]; playerCell: CellProps } {
   const randomCells = shuffleArray(movableCells);
 
+  const cells = fixedCells.map((o) => {
+    if (o !== undefined) {
+      return o;
+    } else {
+      const randomCell = randomCells.pop();
+
+      if (randomCell === undefined) {
+        throw new Error("uh, i guess something is screwed up with the board dimensions?");
+      }
+
+      return { ...randomCell, rotation: directions[getRandomInteger(0, 3)] };
+    }
+  });
+
+  return { cells: cells, playerCell: { ...randomCells.pop()!, rotation: 0 } };
+}
+
+const Board = () => {
+  const { cells, playerCell } = getRandomBoardCells();
+  console.debug(playerCell);
+
   return (
-    <div className="board">
-      {fixedCells.map((o, i) => {
-        return o !== undefined ? (
+    <div>
+      <div style={{ width: 100, margin: 20 }}>
+        <CellMemoized {...playerCell} />
+      </div>
+      <div className="board">
+        {cells.map((o, i) => (
           <CellMemoized key={i} {...o} />
-        ) : (
-          <CellMemoized key={i} {...randomCells.pop()!} rotation={directions[getRandomInteger(0, 3)]} />
-        );
-      })}
+        ))}
+      </div>
     </div>
   );
 };
--- frontend/src/board/Cells.tsx
+++ frontend/src/board/Cells.tsx
@@ -1,12 +1,45 @@
 import { CellProps, ICell, LCell, TCell } from "./Cell";
+import { ItemCellMemoized } from "./ItemCell";
 import { StartingCellMemoized } from "./StartingCell";
 
+export const things = [
+  "genie",
+  "ring",
+  "map",
+  "ghost",
+  "rat",
+  "dragon",
+  "troll",
+  "candles",
+  "chest",
+  "crown",
+  "book",
+  "lizzard",
+  "bat",
+  "owl",
+  "moth",
+  "spider",
+  "armor",
+  "emerald",
+  "keys",
+  "moneysack",
+  "elf",
+  "scarab",
+  "sword",
+  "skull",
+] as const;
+
+export type Thing = (typeof things)[number];
+
+/**
+ * These are the fixes cells which cannot be moved
+ */
 export const fixedCells: readonly (CellProps | undefined)[] = [
   { openings: LCell, rotation: 90, content: <StartingCellMemoized color="green" /> },
   undefined,
-  { openings: TCell, rotation: 180 },
+  { openings: TCell, rotation: 180, content: <ItemCellMemoized item="genie" /> },
   undefined,
-  { openings: TCell, rotation: 180 },
+  { openings: TCell, rotation: 180, content: <ItemCellMemoized item="map" /> },
   undefined,
   { openings: LCell, rotation: 180, content: <StartingCellMemoized color="red" /> },
   undefined,
@@ -16,27 +49,27 @@ export const fixedCells: readonly (CellProps | undefined)[] = [
   undefined,
   undefined,
   undefined,
-  { openings: TCell, rotation: 90 },
+  { openings: TCell, rotation: 90, content: <ItemCellMemoized item="candles" /> },
   undefined,
-  { openings: TCell, rotation: 90 },
+  { openings: TCell, rotation: 90, content: <ItemCellMemoized item="chest" /> },
   undefined,
-  { openings: TCell, rotation: 180 },
+  { openings: TCell, rotation: 180, content: <ItemCellMemoized item="crown" /> },
   undefined,
-  { openings: TCell, rotation: 270 },
-  undefined,
-  undefined,
+  { openings: TCell, rotation: 270, content: <ItemCellMemoized item="book" /> },
   undefined,
   undefined,
   undefined,
   undefined,
   undefined,
-  { openings: TCell, rotation: 90 },
   undefined,
-  { openings: TCell, rotation: 0 },
   undefined,
-  { openings: TCell, rotation: 270 },
+  { openings: TCell, rotation: 90, content: <ItemCellMemoized item="armor" /> },
   undefined,
-  { openings: TCell, rotation: 270 },
+  { openings: TCell, rotation: 0, content: <ItemCellMemoized item="emerald" /> },
+  undefined,
+  { openings: TCell, rotation: 270, content: <ItemCellMemoized item="keys" /> },
+  undefined,
+  { openings: TCell, rotation: 270, content: <ItemCellMemoized item="moneysack" /> },
   undefined,
   undefined,
   undefined,
@@ -46,20 +79,25 @@ export const fixedCells: readonly (CellProps | undefined)[] = [
   undefined,
   { openings: LCell, rotation: 0, content: <StartingCellMemoized color="blue" /> },
   undefined,
-  { openings: TCell, rotation: 0 },
+  { openings: TCell, rotation: 0, content: <ItemCellMemoized item="sword" /> },
   undefined,
-  { openings: TCell, rotation: 0 },
+  { openings: TCell, rotation: 0, content: <ItemCellMemoized item="skull" /> },
   undefined,
   { openings: LCell, rotation: 270, content: <StartingCellMemoized color="yellow" /> },
 ];
 
+/**
+ * All the other cells which can me moved and rotated
+ * Note that one is left out for the player
+ */
 export const movableCells: readonly (Omit<CellProps, "rotation"> | undefined)[] = [
+  { openings: TCell, content: <ItemCellMemoized item="ring" /> },
+  { openings: TCell, content: <ItemCellMemoized item="ghost" /> },
+  { openings: TCell, content: <ItemCellMemoized item="dragon" /> },
+  { openings: TCell, content: <ItemCellMemoized item="bat" /> },
+  { openings: TCell, content: <ItemCellMemoized item="elf" /> },
   { openings: TCell },
-  { openings: TCell },
-  { openings: TCell },
-  { openings: TCell },
-  { openings: TCell },
-  { openings: TCell },
+  { openings: TCell }, // hmm is this the correct one? count them...
 
   { openings: ICell },
   { openings: ICell },
@@ -73,11 +111,11 @@ export const movableCells: readonly (Omit<CellProps, "rotation"> | undefined)[] 
   { openings: ICell },
   { openings: ICell },
 
-  { openings: LCell },
-  { openings: LCell },
-  { openings: LCell },
-  { openings: LCell },
-  { openings: LCell },
+  { openings: LCell, content: <ItemCellMemoized item="lizzard" /> },
+  { openings: LCell, content: <ItemCellMemoized item="owl" /> },
+  { openings: LCell, content: <ItemCellMemoized item="moth" /> },
+  { openings: LCell, content: <ItemCellMemoized item="spider" /> },
+  { openings: LCell, content: <ItemCellMemoized item="scarab" /> },
   { openings: LCell },
   { openings: LCell },
   { openings: LCell },
@@ -93,7 +131,7 @@ export const movableCells: readonly (Omit<CellProps, "rotation"> | undefined)[] 
 
 /*
 
-6 * T
+6 or 7? * T
 16 * fixed
 11 * I
 16 * L
