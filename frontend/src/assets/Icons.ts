c664e5f985c4a382ad991bf1ace889786b2d647a
Verner Fortelius <verner@fortelius.net>
2024-08-29 22:15:27 +0300

    add some icons

---
+++ frontend/src/assets/Icons.ts
@@ -0,0 +1,10 @@
+import CowIcon from "./cards/the-cow-svgrepo-com.svg?react";
+import BunnyIcon from "./cards/rabbit-svgrepo-com.svg?react";
+import CrocodileIcon from "./cards/crocodile-svgrepo-com.svg?react";
+import MooseIcon from "./cards/elk-svgrepo-com.svg?react";
+import WildBoarIcon from "./cards/wild-boar-svgrepo-com.svg?react";
+import SquirrelIcon from "./cards/squirrel-svgrepo-com.svg?react";
+import RaccoonIcon from "./cards/raccoon-svgrepo-com.svg?react";
+import CrabIcon from "./cards/crab-svgrepo-com.svg?react";
+
+export { CowIcon, BunnyIcon, CrocodileIcon, MooseIcon, WildBoarIcon, SquirrelIcon, RaccoonIcon, CrabIcon };
