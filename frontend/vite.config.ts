f088bda59e4b95fafcdad016c45c3b52bad39335
Verner Fortelius <verner@fortelius.net>
2024-10-10 21:36:50 +0300

    bump

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import pluginChecker from "vite-plugin-checker";
 import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), pluginChecker({ typescript: true })],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
 });
