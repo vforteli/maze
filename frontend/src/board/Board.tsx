1dc90da40619f3654913a0c8356b373d606be69e
Verner Fortelius <verner@fortelius.net>
2024-10-10 21:47:22 +0300

    testing css motion path \o/
    refactor
    
    more testing
    
    hmmm maybe

--- frontend/src/board/Board.tsx
+++ frontend/src/board/Board.tsx
@@ -1,5 +1,5 @@
 import "./Board.scss";
-import React, { useState } from "react";
+import React, { RefObject, useEffect, useLayoutEffect, useRef, useState } from "react";
 import { TileMemoized } from "./tiles/Tile";
 import { BoardState, getRandomBoardTiles, MoveDirection, moveTiles, rotatePlayerTile } from "./boardUtils";
 import { CardStackMemoized } from "./cards/CardStack";
@@ -15,13 +15,11 @@ const Edge = ({
   boardState: BoardState;
   onClick: (i: number) => void;
 }) => {
-  const count = direction === "down" || direction === "up" ? boardState.tiles.length : boardState.tiles[0].length;
-
   return (
     <div className={className}>
-      {[...Array(count).keys()].map((i) =>
+      {[...Array(direction === "down" || direction === "up" ? boardState.tiles.length : boardState.tiles[0].length).keys()].map((i) =>
         i % 2 !== 0 ? (
-          <div className="player-tile movable" onClick={() => onClick(i)}>
+          <div key={i} className="player-tile movable" onClick={() => onClick(i)}>
             <TileMemoized {...boardState.playerTile} key={boardState.playerTile.id} />
           </div>
         ) : (
@@ -32,15 +30,85 @@ const Edge = ({
   );
 };
 
+const MovableTile = ({
+  shift,
+  move,
+  targetRef,
+  children,
+  onAnimationEnd,
+}: {
+  shift: boolean;
+  move: boolean;
+  targetRef: RefObject<HTMLDivElement>;
+  children: React.ReactNode;
+  onAnimationEnd: () => void;
+}) => {
+  // all of this seems highly inefficient, although i guess if efficiency was a thing i wouldnt be doing this in css and react :D
+  const ref = useRef<HTMLDivElement>(null);
+  const [path, setPath] = useState<string | undefined>(undefined);
+
+  useLayoutEffect(() => {
+    if (ref.current !== null && targetRef.current !== null) {
+      if (shift) {
+        const animationOptions: KeyframeAnimationOptions = {
+          duration: 1000,
+          easing: "ease-in-out",
+          composite: "replace",
+          fill: "forwards",
+        };
+
+        const currentRect = ref.current.getBoundingClientRect();
+        const targetRect = targetRef.current?.getBoundingClientRect();
+
+        // offsets to move the piece one piece length in... some direction
+        // obviously this should check direction... using right for testing
+        const dx_shift = Math.round(currentRect.width + 3);
+        const dy_shift = 0;
+
+        // these are the offset from the current tile to the player tile
+        const dx_playertile = Math.round(targetRect.x - currentRect.x);
+        const dy_playertile = Math.round(targetRect.y - currentRect.y);
+
+        setPath(`path("M0,0 L${dx_shift},${dy_shift} L${dx_playertile},${dy_playertile}")`);
+
+        const shiftAnimation = ref.current.animate([{ offsetDistance: "0%" }, { offsetDistance: `${dx_shift}px` }], { ...animationOptions, duration: 500 });
+        shiftAnimation.addEventListener("finish", () => {
+          if (move) {
+            const moveAnimation = ref.current!.animate([{ offsetDistance: `${dx_shift}px` }, { offsetDistance: "100%" }], animationOptions);
+            moveAnimation.addEventListener("finish", () => {
+              onAnimationEnd();
+            });
+          }
+        });
+      } else {
+        setPath(undefined);
+      }
+    }
+  }, [targetRef, ref, shift, move, onAnimationEnd]);
+
+  return (
+    <div className="movable-tile" style={{ offsetPath: path }} ref={ref}>
+      {children}
+    </div>
+  );
+};
+
 const Board = () => {
   const [boardState, setBoardState] = useState(getRandomBoardTiles());
+  const playerTileRef = useRef<HTMLDivElement>(null);
+  const [moveRowIndex, setMoveRowIndex] = useState<number | undefined>(undefined);
 
   const handleMoveTiles = (index: number, direction: MoveDirection) => {
-    setBoardState((b) => moveTiles(b, index, direction));
+    setMoveRowIndex(direction === "right" ? index : undefined);
   };
 
   const handleRotatePlayerTile = () => {
     setBoardState((s) => rotatePlayerTile(s));
+  };
+
+  const handleMoveTilesAnimationEnd = (index: number, direction: MoveDirection) => {
+    setMoveRowIndex(undefined);
+    setBoardState((b) => moveTiles(b, index, direction));
   };
 
   return (
@@ -49,7 +117,7 @@ const Board = () => {
 
       <div className="player-tile-container">
         <div style={{ width: 80, margin: 20 }}>
-          <div className="player-tile" onClick={() => handleRotatePlayerTile()}>
+          <div ref={playerTileRef} className="player-tile" onClick={() => handleRotatePlayerTile()}>
             <TileMemoized {...boardState.playerTile} key={boardState.playerTile.id} />
           </div>
         </div>
@@ -61,7 +129,21 @@ const Board = () => {
         <Edge className="frame-side top" direction="down" boardState={boardState} onClick={(i) => handleMoveTiles(i, "down")} />
         <Edge className="frame-side bottom" direction="up" boardState={boardState} onClick={(i) => handleMoveTiles(i, "up")} />
         <div className="board">
-          {boardState.tiles.flatMap((o, rowIndex) => o.map((o, columnIndex) => <TileMemoized key={`${columnIndex}_${rowIndex}`} {...o} />))}
+          {boardState.tiles.flatMap((o, rowIndex) =>
+            o.map((o, columnIndex) => {
+              return (
+                <MovableTile
+                  move={moveRowIndex === rowIndex && columnIndex === 6}
+                  shift={moveRowIndex === rowIndex}
+                  key={`${columnIndex}_${rowIndex}`}
+                  targetRef={playerTileRef}
+                  onAnimationEnd={() => handleMoveTilesAnimationEnd(rowIndex, "right")}
+                >
+                  <TileMemoized {...o} />
+                </MovableTile>
+              );
+            }),
+          )}
         </div>
       </div>
     </div>
